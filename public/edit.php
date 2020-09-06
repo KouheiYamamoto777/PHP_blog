01b57b5d8ab3647c0e38a9a9cba4158416fd0fb9
KouheiYamamoto777 <kouheiyamamoto777@gmail.com>
2020-09-06 14:23:51 +0900

    radioボタンのphp文を変更しました
    渡されたpostデータの型が文字列型から数値型に変更されたので、if文を変更しました

--- public/edit.php
+++ public/edit.php
@@ -20,14 +20,14 @@ $story = $blogContent->getBlogContent($_GET);
         <p>タイトル : </p>
         <input type="text" name="title" value="<?= h($story['title']) ?>">
         <p>カテゴリ : </p>
-        <input type="radio" name="category" value="1" <?php if($story['category'] === '1') { echo 'checked';} ?>>ブログ
-        <input type="radio" name="category" value="2" <?php if($story['category'] === '2') { echo 'checked';} ?>>日常
-        <input type="radio" name="category" value="3" <?php if($story['category'] === '3') { echo 'checked';} ?>>プログラミング
+        <input type="radio" name="category" value="1" <?php if($story['category'] === 1) { echo 'checked';} ?>>ブログ
+        <input type="radio" name="category" value="2" <?php if($story['category'] === 2) { echo 'checked';} ?>>日常
+        <input type="radio" name="category" value="3" <?php if($story['category'] === 3) { echo 'checked';} ?>>プログラミング
         <p>ブログ本文 : </p>
         <textarea name="content" id="" cols="30" rows="10"><?= $story['content'] ?></textarea>
         <p>公開ステータス : </p>
-        <input type="radio" name="publish_status" value="1" <?php if($story['publish_status'] === '1') { echo 'checked';} ?>>公開
-        <input type="radio" name="publish_status" value="2" <?php if($story['publish_status'] === '2') { echo 'checked';} ?>>非公開
+        <input type="radio" name="publish_status" value="1" <?php if($story['publish_status'] === 1) { echo 'checked';} ?>>公開
+        <input type="radio" name="publish_status" value="2" <?php if($story['publish_status'] === 2) { echo 'checked';} ?>>非公開
         <input type="hidden" name="id" value="<?= h($story['id']) ?>">
         <p><input type="submit" value="変更する"></p>
     </form>
